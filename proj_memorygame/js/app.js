a3ad75fdb7dd7520ee9acf4dfb548b12ab1e6555
Katie King <kesaxton1@gmail.com>
2018-08-27 21:49:32 -0400

    Updates so far...almost done

--- proj_memorygame/js/app.js
+++ proj_memorygame/js/app.js
@@ -1,31 +1,48 @@
+const cardDeck = document.querySelector(".deck"); // get the container for all the cards
+
+const moveCounter = document.querySelector(".moves"); // get the container for the move counter
+
+const starsContainer = document.querySelector(".stars"); // get the container for the star rating
+
+const timer = document.querySelector(".timer"); // get the container for the timer
+
+const restart = document.querySelector(".restart"); // get the container for the restart icon
+
+const modal = document.querySelector("#final-score");
+
 // create a list that holds all of the card values
 let cardOptions = [
-  "fa fa-diamond",
-  "fa fa-paper-plane-o",
-  "fa fa-anchor",
-  "fa fa-bolt",
-  "fa fa-cube",
-  "fa fa-leaf",
-  "fa fa-bicycle",
-  "fa fa-bomb",
-  "fa fa-diamond",
-  "fa fa-paper-plane-o",
-  "fa fa-anchor",
-  "fa fa-bolt",
-  "fa fa-cube",
-  "fa fa-leaf",
-  "fa fa-bicycle",
-  "fa fa-bomb"];
+  "far fa-gem",
+  "far fa-gem",
+  "fas fa-tree",
+  "fas fa-tree",
+  "far fa-heart",
+  "far fa-heart",
+  "fas fa-bolt",
+  "fas fa-bolt",
+  "far fa-moon",
+  "far fa-moon",
+  "fas fa-paw",
+  "fas fa-paw",
+  "fas fa-crown",
+  "fas fa-crown",
+  "far fa-snowflake",
+  "far fa-snowflake"];
 
-// grab the container for all the cards
-const cardDeck = document.querySelector(".deck");
+let openCards = []; // create an array for tracking open cards
 
-// create an array for tracking open cards
-let openCards = [];
-// create an array for tracking current cards clicked
-let flippedOptions = [];
-// create a variable to track moves
-let moves = 0;
+let flippedOptions = []; // create an array for tracking current cards clicked
+
+let matchedCards = []; // create a variable to track matched cards
+
+let moves = 0; // create a variable to track moves
+
+let currentStars = [];
+
+// create variables to track the time and timer
+let currentTime = 0;
+let finalTime = 0;
+let timerID = null;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -42,57 +59,19 @@ function shuffle(array) {
     return array;
 }
 
-// shuffle the cards
-cardOptions = shuffle(cardOptions);
-
-// define behavior when a card is clicked
-function cardClick(val) {
-  val.addEventListener("click",function(){
-
-    displayCard(this); // display card
-
-    addToOpen(this); // add classes to card
-
-    if (flippedOptions.length === 2) { // when there are two cards open
-
-      openCards = cardDeck.querySelectorAll(".open"); // find the HTML of the open cards
-
-      if (flippedOptions[0] === flippedOptions[1]){  //compare the values of the open cards for a match
-        cardMatch(); // if match
-      } else {
-        cardNoMatch(); // if no match
-      };
-
-      flippedOptions = []; // reset open card values for more attempts
-
-    };
-
-
-    incrementMoves();
-
-
-    // if all cards are matched, pop-up dialog and display score
-      // check for all cards matched
-      matchWinner();
-  });
-};
-
-
-
-
 function displayCard(val){
-  if (!val.classList.contains("match") && !val.classList.contains("open") && flippedOptions.length < 2){ //card clicked is not a match already, nor open already, and there aren't already 2 cards open
-    val.className += " open show"; // display card
-  };
+    if (!val.classList.contains("match") && !val.classList.contains("open")){ //card clicked is not a match already, nor open already
+      val.className += " open show"; // display card
+      addToOpen(val); // add classes to card
+    };
 };
 
 
 function addToOpen(val){
   let targetCard = val.querySelector("i").classList.item(1); // get card class name to use as unique value
+
   flippedOptions.push(targetCard); // add unique value to list of open cards
 };
-
-
 
 
 function cardMatch(){ // when there is a match
@@ -100,6 +79,7 @@ function cardMatch(){ // when there is a match
     card.classList.add("match"); // add match class
     card.classList.remove("open","show"); // remove unnecessary classes
   });
+  matchedCards = cardDeck.querySelectorAll(".match");
 };
 
 
@@ -108,35 +88,82 @@ function cardNoMatch(){ // when there is not a match
     openCards.forEach(function(card){ // for each open card HTML
       card.classList.remove("open","show"); // remove classes to hide card
     });
-
     openCards = []; // reset list of open cards
-  }, 1500); // after 5 seconds
+  }, 600); // after .6 seconds
 };
 
 
 function incrementMoves(){
-  moves += 1; // increment moves taken
-  // based on number of moves taken, remove stars
+  moves++; // increment moves taken
+
+  moveCounter.textContent = moves; // update visible move counter
+
+  currentStars = starsContainer.querySelectorAll("li > i[class='fas fa-star']"); // get current star rating
+
+  if (moves === 17){ // based on number of moves taken, remove stars. This is for two stars
+    currentStars[2].classList.remove('fas', 'fa-star');
+    currentStars[2].classList.add('far', 'fa-star');
+  } else if (moves === 26) { // one star
+    currentStars[1].classList.remove('fas', 'fa-star');
+    currentStars[1].classList.add('far', 'fa-star');
+  };
 };
 
+function matchWinner(){ // when all matches are made
 
-function matchWinner(){
-  // stop timer
-  // congratulations pop-up
-  // display final score (timer and stars)
-  // ask to restart
+  finalTime = currentTime; // save final time
+  clearInterval(timerID); // stop timer
+  currentTime = 0;
+
+  modal.innerHTML = `<h1>Congratulations, you won!</h1><p>Your score: ${currentStars}</p><p>Time taken: ${finalTime} seconds</p><p>Moves taken: ${moves}</p><p><button id="new-game">Play again?</button></p>`;
+
+  let redoButton = document.querySelector("#new-game");   // ask to restart
+  redoButton.addEventListener("click", restartGame);
+
+  modal.addEventListener("click", function(){
+    modal.style.display = "none";
+  });
+
+  modal.style.display = "block";
 };
 
-function restart(){
+function incrementClock(){
+  cardDeck.removeEventListener("click", incrementClock); // prevent function from running more than once
+
+  timerID = setInterval(function(){
+    currentTime += 1; // increment time in seconds
+    timer.textContent = currentTime; // update visible timer
+  }, 1000); // every second
+};
+
+function restartGame(){
+
+  clearInterval(timerID);
+  currentTime = 0; // reset timers
+  finalTime = 0;
+
+  matchedCards = []; // reset list of matched cards
+
+  moves = 0; // reset moves
+
   initialize();
-  //reset timer
-  //reset stars
-  //reset moves
 };
 
 
-// prepare the game
-function initialize() {
+function initialize() { // prepare the game
+
+  modal.style.display = "none";
+
+  cardDeck.innerHTML = ``; // delete any existing cards
+
+  moveCounter.textContent = 0; // display 0 moves
+
+  starsContainer.innerHTML = `<li><i class="fas fa-star"></i></li><li><i class="fas fa-star"></i></li><li><i class="fas fa-star"></i></li>`; // build star rating
+
+  timer.textContent = 0; // display 0 time
+
+  cardOptions = shuffle(cardOptions); // shuffle the cards
+
   for (let i = 0; i < cardOptions.length; i++){ // perform function on each value in the card list
 
     let card = document.createElement('li'); // create a list item element and store it in a variable
@@ -151,22 +178,40 @@ function initialize() {
 
   };
 
+  cardDeck.addEventListener("click", incrementClock);   // on first click, start timer
+
+  restart.addEventListener("click", restartGame);   // when the restart icon is clicked
+
+};
+
+function cardClick(val) {
+  val.addEventListener("click",function(){ // define behavior when a card is clicked
+
+    if (flippedOptions.length < 2){ // if there are less than 2 cards open
+      displayCard(this); // display card
+      incrementMoves(); // increment moves and adjust stars
+    };
+
+    if (flippedOptions.length === 2) { // when there are two cards open
+
+      openCards = cardDeck.querySelectorAll(".open"); // find the HTML of the open cards
+
+      if (flippedOptions[0] === flippedOptions[1]){  //compare the values of the open cards for a match
+        cardMatch(); // if match
+      } else {
+        cardNoMatch(); // if no match
+      };
+
+      flippedOptions = []; // clear open card values for more attempts
+
+    };
+
+    if (matchedCards.length === 16){ // check for all cards matched
+      matchWinner();
+    };
+  });
+
 };
 
 // run the prep function
 initialize();
-
-// on first card click, start timer
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
