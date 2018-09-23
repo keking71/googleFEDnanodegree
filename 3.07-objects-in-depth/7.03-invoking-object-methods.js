3db33332f79b27a65038e70daf9ab5fcf1f05266
Katie King <kesaxton1@gmail.com>
2018-09-23 09:58:43 -0400

    Using 'this' in an object function

---
+++ 3.07-objects-in-depth/7.03-invoking-object-methods.js
@@ -0,0 +1,22 @@
+/*
+
+Create an object called `chameleon` with two properties:
+
+1. `color`, whose value is initially set to 'green' or 'pink'
+2. `changeColor`, a function which changes `chameleon`'s `color` to 'pink'
+    if it is 'green', or to 'green' if it is 'pink'
+
+*/
+let chameleon = {
+    color: 'pink',
+    changeColor: function() {
+        if (this.color === 'pink'){
+            this.color = 'green';
+        } else {
+            this.color = 'pink';
+        }
+    }
+};
+
+chameleon.changeColor();
+console.log(chameleon.color);
